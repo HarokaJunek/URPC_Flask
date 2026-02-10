242132331a1aecf361dfa735702f43dbf1d47b3f
Tanchik <gaiduk.ta.a@gmail.com>
2026-02-10 22:32:00 +0300

    подключила бд

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import sqlite3
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+#подключение бд(таня)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nagruzka_DEMO.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+#таня
 
 @app.route('/')
 def index():
